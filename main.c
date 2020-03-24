865af21e77ecaf13d64cc786d848ee3d5fe8a353
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-03-24 01:32:42 -0400

    Update main.c

--- main.c
+++ main.c
@@ -15,6 +15,34 @@ void addBlock(int data){
         head->blockData = data;
         return;
     }
+    struct block *currentBlock = head;
+    while(currentBlock->link)
+        currentBlock = currentBlock->link;
+    struct block *newBlock = malloc(sizeof(struct block));
+    currentBlock->link = newBlock;
+    newBlock->blockData = data;
+    SHA256(toString("*currentBlock"), sizeof(*currentBlock), newBlock->prevHash);
+}
+
+void verifyChain(){
+    if(head == NULL){
+        printf("Blockchain is empty! Try again after adding blocks");
+        return;
+    }
+    struct block *curr = head->link, *prev = head;
+    int count = 1;
+    while(curr){
+        printf("%d\t[%d]\t", count++, curr->blockData);
+        hashPrinter(SHA256(toString(*prev), sizeof(*prev), NULL));
+        printf("\n");
+        hashPrinter(curr->prevHash, SHA256_DIGEST_LENGTH);
+        if(hashCompare(SHA256(toString(*prev), sizeof(*prev), NULL), curr->prevHash))
+            printf("Verification has succeeded!\n");
+        else
+            printf("Verification has failed.\n");
+        prev = curr;
+        curr = curr->link;
+    }
 }
 
 int main(){
