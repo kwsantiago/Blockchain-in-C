1a6239d936f8d2de747c1d9f657e342ea15b959f
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-23 20:06:43 -0400

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,32 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
+    self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
         # Creates a new block and adds to chain
-        pass
-    
+        
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1],
+        }
+
+        # Reset the current list of tx
+        self.current_transactions = []
+
+        self.chain.append(block)
+            return block
+
     def new_transaction(self, sender, recipient, amount):
         # Adds a new tx to the list of tx
         self.current_transactions.append({
@@ -18,9 +34,13 @@ class Blockchain(object):
             'recipient': recipient,
             'amount': amount,
         })
-        
+
         return self.last_block['index'] + 1
     
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
     @staticmethod
     def hash(block):
         # Hashes a block
@@ -30,5 +50,17 @@ class Blockchain(object):
     def last_block(self):
         # Returns the last block in the chain
         pass
-        
-        
+    
+    def proof_of_work(self, last_proof):
+
+    proof = 0
+    while self.valid_proof(last_proof, proof) is False:
+        proof += 1
+    return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+    
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:4] == "0000"
